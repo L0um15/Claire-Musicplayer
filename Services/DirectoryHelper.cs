99f6dfa6e9d61259a6635705a827f34c910723c9
L0um15 <damian.ziolo@onet.pl>
2021-01-30 11:52:38 +0100

    Create DirectoryHelper.cs

---
+++ Services/DirectoryHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Claire_Musicplayer.Services
+{
+    public static class DirectoryHelper
+    {
+        public static string CurrentDirectory { get; private set; } = Directory.GetCurrentDirectory();
+
+        /// <summary>
+        /// Will attempt to change directory and handle DirectoryNotFoundException
+        /// </summary>
+        /// <param name="path">Can be absolute or relative path</param>
+        public static void TrySetCurrentDirectory(string path)
+        {
+            try
+            {
+                if (Path.IsPathRooted(path))
+                {
+                    Directory.SetCurrentDirectory(path);
+                    CurrentDirectory = Directory.GetCurrentDirectory();
+                }
+                else
+                {
+                    Directory.SetCurrentDirectory(Path.Combine(CurrentDirectory, path));
+                    CurrentDirectory = Directory.GetCurrentDirectory();
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageExtensions.WriteLine($"Directory: \"{path}\" not found");
+            }
+        }
+
+    }
+}
